cfe54d0a0cc75e630e474cca0cbc443a29ed5801
Alvaro <alvaro_benja_iriartemac87@hotmail.com>
2024-10-06 11:31:52 -0300

    "hast tash"

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import Contacts from '../views/Contacts.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/',
