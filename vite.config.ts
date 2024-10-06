2404a4ec6acacf90007e90173f66b25c0e296935
Alvaro <alvaro_benja_iriartemac87@hotmail.com>
2024-10-06 11:09:20 -0300

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base:'https://alvaro-iriarte87.github.io/estetica-figueroa-page',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
